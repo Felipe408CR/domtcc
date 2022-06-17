7713d15168c32deae80a5b7936d966aeb71137d8
alejandra garia <aljanddfj@4565.com>
2022-06-17 12:15:15 -0500

    Se introducen productos

--- src/js/controladorIndex.js
+++ src/js/controladorIndex.js
@@ -1,9 +1,121 @@
-//ENTRADAS == VARIABLES
-var nombre="JAMES RODRIGUEZ"
 
-if(true){
-    var nombre="Lucho diaz"
+let producto1 = {
+    nombre: 'Bugatti Chiron',
+    precio: 4301450, 
+    cantidad: '1',
+    foto: 'url',
+    descripcion: 'Bugatti Chiron S.P'
 }
-console.log(`hola ${nombre}`)
-console.log("hola")
-console.log("adios")
+
+console.log(producto1)
+console.log(producto1.nombre)
+
+let producto2 = {
+    nombre: 'Bugatti Chiron',
+    precio: 4301450, 
+    cantidad: '1',
+    foto: 'url',
+    descripcion: 'Bugatti Chiron S.P'
+}
+
+console.log(producto2)
+console.log(producto2.nombre)
+
+let producto3 = {
+    nombre: 'Bugatti Chiron',
+    precio: 4301450, 
+    cantidad: '1',
+    foto: 'url',
+    descripcion: 'Bugatti Chiron S.P'
+}
+
+console.log(producto3)
+console.log(producto3.nombre)
+
+let producto4 = {
+    nombre: 'Bugatti Chiron',
+    precio: 4301450, 
+    cantidad: '1',
+    foto: 'url',
+    descripcion: 'Bugatti Chiron S.P'
+}
+
+console.log(producto4)
+console.log(producto4.nombre)
+
+let producto5 = {
+    nombre: 'Bugatti Chiron',
+    precio: 4301450, 
+    cantidad: '1',
+    foto: 'url',
+    descripcion: 'Bugatti Chiron S.P'
+}
+
+console.log(producto5)
+console.log(producto5.nombre)
+
+let producto6 = {
+    nombre: 'Bugatti Chiron',
+    precio: 4301450, 
+    cantidad: '1',
+    foto: 'url',
+    descripcion: 'Bugatti Chiron S.P'
+}
+
+console.log(producto6)
+console.log(producto6.nombre)
+
+let producto7 = {
+    nombre: 'Bugatti Chiron',
+    precio: 4301450, 
+    cantidad: '1',
+    foto: 'url',
+    descripcion: 'Bugatti Chiron S.P'
+}
+
+console.log(producto7)
+console.log(producto7.nombre)
+
+let producto8 = {
+    nombre: 'Bugatti Chiron',
+    precio: 4301450, 
+    cantidad: '1',
+    foto: 'url',
+    descripcion: 'Bugatti Chiron S.P'
+}
+
+console.log(producto8)
+console.log(producto8.nombre)
+
+let producto9 = {
+    nombre: 'Bugatti Chiron',
+    precio: 4301450, 
+    cantidad: '1',
+    foto: 'url',
+    descripcion: 'Bugatti Chiron S.P'
+}
+
+console.log(producto9)
+console.log(producto9.nombre)
+
+let producto10 = {
+    nombre: 'Bugatti Chiron',
+    precio: 4301450, 
+    cantidad: '1',
+    foto: 'url',
+    descripcion: 'Bugatti Chiron S.P'
+}
+
+console.log(producto10)
+console.log(producto10.nombre)
+
+let producto11 = {
+    nombre: 'Bugatti Chiron',
+    precio: 4301450, 
+    cantidad: '1',
+    foto: 'url',
+    descripcion: 'Bugatti Chiron S.P'
+}
+
+console.log(producto11)
+console.log(producto11.nombre)
